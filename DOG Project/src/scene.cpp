1fcbea1823ffbca796f7b96f3b9b753a4f63e4d9
clnetosales <clnetosales@1481cf19-3fed-db61-1505-0acbd70bc139>
2010-09-02 01:10:31 +0000

    Versão Inicial - Esboço da classe do SDL estática, da parte comum do projeto em util.h e um protótipo da classe Scene genérica

---
+++ DOG Project/src/scene.cpp
@@ -0,0 +1,30 @@
+/*
+ * scene.cpp
+ *
+ *  Created on: 01/09/2010
+ *      Author: Cleóbulo
+ */
+
+#include "scene.h"
+
+Scene::Scene() { running = true; }
+
+SceneMessage Scene::run(){
+
+	// Read necessary data
+	load();
+
+	// Main loop
+	while( Scene::running ){
+
+		inputs();
+		logic();
+		render();
+	}
+
+	// Destroy the unecessary data
+	unload();
+
+	// Return the next request
+	return result();
+}
