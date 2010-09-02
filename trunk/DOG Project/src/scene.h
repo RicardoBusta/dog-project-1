f7f0533acb5287000c89eb6245c1145c3d7c1506
clnetosales <clnetosales@1481cf19-3fed-db61-1505-0acbd70bc139>
2010-09-02 01:10:31 +0000

    Versão Inicial - Esboço da classe do SDL estática, da parte comum do projeto em util.h e um protótipo da classe Scene genérica

---
+++ trunk/DOG Project/src/scene.h
@@ -0,0 +1,44 @@
+/*
+ * scene.h
+ *	A base para os estados do jogo, encapsula o loop principal
+ *
+ *  Created on: 01/09/2010
+ *      Author: Cleóbulo
+ */
+
+#ifndef SCENE_H_
+#define SCENE_H_
+
+#include "util.h"
+#include "sdl.h"
+
+class Scene {
+public:
+	Scene();
+	virtual ~Scene();
+
+	// Data preparation
+	virtual bool load();
+	virtual bool prepare();
+	virtual bool unload();
+
+	// Main call
+	SceneMessage run();
+
+	// GameLoop
+	void inputs();
+	virtual void logic();
+	virtual void render();
+
+	// Result of the scene
+	virtual SceneMessage result();
+
+	// Play a subscene
+	/*static SceneMessage playScene( Scene* , LoopState );
+	static SceneMessage playScene( SceneMessage );*/
+
+protected:
+	bool running;
+};
+
+#endif /* SCENE_H_ */
