4c3de561986a666fd68b5663d693e6c837185584
fernandodsl <fernandodsl@1481cf19-3fed-db61-1505-0acbd70bc139>
2010-09-19 01:32:29 +0000

    Primeria versão do ContentManager.

---
+++ DoGProject/src/contents/ContentManager.cpp
@@ -0,0 +1,57 @@
+/*
+ * ContentManager.cpp
+ *
+ *  Created on: 18/09/2010
+ *      Author: fernando
+ */
+
+#include "ContentManager.h"
+
+list<Content*> ContentManager::contents;
+list<Content*>::iterator ContentManager::it;
+
+// Retorna um content, NULL caso inexistente
+Content* ContentManager::getContent(CONTENT_ type, std::string &label)
+{
+	bool success = searchContent(type, label);
+	if(success)
+		return *it;
+	return NULL;
+}
+
+bool ContentManager::removeContent(CONTENT_ type, std::string &label)	// Remove um content, false caso nao remova
+{
+	bool success = searchContent(type, label);
+	if(success && (*it)->used() == false)
+	{
+		contents.erase(it);
+		return true;
+	}
+	return false;
+}
+// Adiciona um content no fim da lista, true sucesso, false falhou
+bool ContentManager::addContent(Content *content)
+{
+	bool found = searchContent(content->getType(), content->getLabel());
+	if(content and found == false)
+	{
+		contents.push_back(content);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// Busca um Content, NULL caso inexistente
+bool ContentManager::searchContent(CONTENT_ type, std::string label)
+{
+	for (it = contents.begin(); it != contents.end(); it++)
+	{
+		if(type == (*it)->getType() && label.compare((*it)->getLabel()))
+			return true;
+	}
+	return false;
+}
+
---
+++ DoGProject/src/contents/ContentManager.h
@@ -0,0 +1,29 @@
+/*
+ * ContentManager.h
+ *
+ *  Created on: 18/09/2010
+ *      Author: fernando
+ */
+
+#ifndef CONTENTMANAGER_H_
+#define CONTENTMANAGER_H_
+
+#include "content.h"
+#include <list>
+#include <string>
+
+class ContentManager {
+public:
+
+	static Content* getContent(CONTENT_, std::string &);	// Retorna um content, NULL caso inexistente
+	static bool removeContent(CONTENT_, std::string &);	// Remove um content, false caso nao remova
+	static bool addContent(Content *);		// Recebe o tipo e a string, falso caso nao adicione
+	static bool searchContent(CONTENT_, std::string);	// Busca um Content, true ou false, armazena em it
+
+private:
+	static list<Content*> contents;
+	static list<Content*>::iterator it;	// Iterator auxiliar usado em search
+
+};
+
+#endif /* CONTENTMANAGER_H_ */
