283225e54da9a72a532c32fc76d074d171ecee97
ricardobqueiroz@gmail.com <ricardobqueiroz@gmail.com@1481cf19-3fed-db61-1505-0acbd70bc139>
2010-09-19 05:02:30 +0000

    Modifiquei a cor do projétil :P

--- DoGProject/src/entities/projectile.cpp
+++ DoGProject/src/entities/projectile.cpp
@@ -21,27 +21,22 @@ Projectile::~Projectile() {
 void Projectile::draw(){
 	glScalef(0.2,0.2,0.2);
 	glBegin( GL_TRIANGLE_FAN );
-			glColor3f( 0.4 , 0.4 , 0.4 );
+			glColor3f( 1 , 1 , 0 );
 			glVertex3f(  0,
 						 0,
 						-25.0f );
-			glColor3f( 0.7 , 0.7 , 0.7 );
 			glVertex3f( -25.0f,
 						 25.0f,
 						 25.0f);
-			glColor3f( 0.6 , 0.6 , 0.6 );
 			glVertex3f(  25.0f,
 						 25.0f,
 						 25.0f);
-			glColor3f( 0.5 , 0.5 , 0.5 );
 			glVertex3f(  25.0f,
 						-25.0f,
 						 25.0f);
-			glColor3f( 0.8 , 0.8 , 0.8 );
 			glVertex3f( -25.0f,
 						-25.0f,
 						 25.0f);
-			glColor3f( 0.7 , 0.7 , 0.7 );
 			glVertex3f( -25.0f,
 						 25.0f,
 						 25.0f);
@@ -49,7 +44,7 @@ void Projectile::draw(){
 
 		// The bottom of the pyramid
 		glBegin( GL_QUADS );
-
+			glColor3f(1,0.5,0);
 			glVertex3f( -25.0f,
 						-25.0f,
 						 25.0f);
