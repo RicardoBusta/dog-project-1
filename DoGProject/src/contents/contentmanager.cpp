5395b670a8c188d6c912e3c90c0a270e97eed3a6
fernandodsl <fernandodsl@1481cf19-3fed-db61-1505-0acbd70bc139>
2010-09-19 20:29:10 +0000

    Estrutura de dados de ContentManager atualizada. Agora usa um unordered_map(tabela hash).

--- DoGProject/src/contents/contentmanager.cpp
+++ DoGProject/src/contents/contentmanager.cpp
@@ -7,15 +7,15 @@
 
 #include "contentmanager.h"
 
-list<Content*> ContentManager::contents;
-list<Content*>::iterator ContentManager::it;
+unordered_map<std::string, Content *> ContentManager::contents;
+unordered_map<std::string, Content *>::iterator ContentManager::it;
 
 // Retorna um content, NULL caso inexistente
 Content* ContentManager::getContent(CONTENT_ type, std::string label)
 {
 	bool success = searchContent(type, label);
 	if(success)
-		return *it;
+		return it->second;
 	return NULL;
 }
 
@@ -24,47 +24,45 @@ bool ContentManager::removeContent(CONTENT_ type, std::string label)	// Remove u
 	bool success = searchContent(type, label);
 	if(success)
 	{
-		if((*it)->used() == false)
+		if(it->second->used() == false)
 		{
 			contents.erase(it);
 			return true;
 		}
 		else
 		{
-			(*it)->removeUser();
+			it->second->removeUser();
 			return false;
 		}
 	}
 	return false;
 }
 // Adiciona um content no fim da lista, true sucesso, false falhou
-bool ContentManager::addContent(Content *content)
+Content* ContentManager::addContent(Content *content)
 {
 	bool found = searchContent(content->getType(), content->getLabel());
 	if(content)
 	{
 		if(found == false)
 		{
-			contents.push_back(content);
-			return true;
+			contents[content->getLabel().c_str()] = content;
+			return content;					// Adicionou o próprio, retorna ele
 		}
-		else
+		else								// Já existe esse content
 		{
-			if(content) (*it)->addUser();
-			return false;
+			if(content) it->second->addUser();
+			return it->second;
 		}
 	}
-	return false;
+	return NULL;
 }
 
-// Busca um Content, NULL caso inexistente
+// Busca um Content, true caso acha, false caso não. Preenche o iterator com o content.
 bool ContentManager::searchContent(CONTENT_ type, std::string label)
 {
-	for (it = contents.begin(); it != contents.end(); it++)
-	{
-		if(type == (*it)->getType() && label == (*it)->getLabel())
-			return true;
-	}
-	return false;
+	it = contents.find(label.c_str());
+	if(it != contents.end())
+		return true;
+	else
+		return false;
 }
-
--- DoGProject/src/contents/contentmanager.h
+++ DoGProject/src/contents/contentmanager.h
@@ -4,30 +4,46 @@
  *  Created on: 18/09/2010
  *      Author: fernando
  */
+#define _DEFINE_DEPRECATED_HASH_CLASSES 0
+
+#ifdef WIN
+#include <unordered_map>
+#else
+#include <tr1/unordered_map>
+using namespace std::tr1;
+#endif
 
 #ifndef CONTENTMANAGER_H_
 #define CONTENTMANAGER_H_
 
 #include "content.h"
-#include <list>
 #include <string>
+using namespace std;
 
 #include "texture.h"
 #include "model.h"
 #include "soundeffect.h"
 
+struct compKey
+{
+	bool operator()(const char* s1, const char* s2) const
+	{
+		return strcmp(s1,s2)==0;
+	}
+};
+
 class ContentManager {
 public:
-
+	static Content* addContent(Content *);		// Recebe o tipo e a string, retorna o content, null caso content null
 	static Content* getContent(CONTENT_, std::string );	// Retorna um content, NULL caso inexistente
 	static bool removeContent(CONTENT_, std::string );	// Remove um content, false caso nao remova
-	static bool addContent(Content *);		// Recebe o tipo e a string, falso caso nao adicione
-	static bool searchContent(CONTENT_, std::string);	// Busca um Content, true ou false, armazena em it
 
 private:
-	static list<Content*> contents;
-	static list<Content*>::iterator it;	// Iterator auxiliar usado em search
+	static inline bool searchContent(CONTENT_, std::string);	// Busca um Content, true ou false, armazena em it
 
+	static unordered_map<std::string, Content *> contents;
+	static unordered_map<std::string, Content *>::iterator it;
 };
+
 
 #endif /* CONTENTMANAGER_H_ */
